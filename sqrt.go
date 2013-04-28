02d50b8e69cdb034076eb10759e09ed1a9920b35
AR Dahal <dahalaishraj@gmail.com>
2013-04-28 14:16:47 +0530

    First two baby programs

---
+++ sqrt.go
@@ -0,0 +1,22 @@
+package main
+ 
+import "fmt"
+ 
+var d = 0.0
+ 
+func Sqrt(x float64) float64 {
+    z := 1.0
+    for d - z != 0 {
+        z = newton(z, x)
+        d = newton(z, x)
+    }
+    return z
+}
+ 
+func newton(z, x float64) float64 {
+    return z - ( ((z*z) - x) / (2*z) )
+}
+ 
+func main() {
+    fmt.Println(Sqrt(4))
+}
