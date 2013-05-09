9e4bffd2cee51b4c94426600ec73cc8b51e06bf4
AR Dahal <dahalaishraj@gmail.com>
2013-05-09 10:33:42 +0545

    HTTP handlers, still buggy

---
+++ toy-examples/http-handlers.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+type String string
+
+type Struct struct {
+    Greeting string
+    Punct string
+    Who string
+}
+
+func (s String) ServeHTTP(
+    w http.ResponseWriter,
+    r *http.Request) {
+        fmt.Fprintf(w,string(s))
+}
+
+func (str Struct) ServeHTTP(
+    w http.ResponseWriter,
+    r *http.Request) {
+        fmt.Fprintf(w,str.Greeting,str.Punct,str.Who)
+}
+
+func main() {
+    http.Handle("/string", String("I'm a frayed knot."))
+    http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+    http.ListenAndServe("localhost:4000",nil)
+}
