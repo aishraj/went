8588164c97f47abe7ef6c2d9696b0572f1eacdd4
AR Dahal <dahalaishraj@gmail.com>
2013-05-12 14:46:00 +0545

    Using packages

--- how-to-write-go/src/github.com/ardahal/hello/hello.go
+++ how-to-write-go/src/github.com/ardahal/hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/ardahal/string"
+)
 
 func main() {
-    fmt.Println("Hello, gopher!")
+    fmt.Println(string.Reverse("Hello, gopher!"))
 }
