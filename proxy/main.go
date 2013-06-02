9b315a32ff87c7b1576ea8afe42032c18c0b3d37
AR Dahal <dahalaishraj@gmail.com>
2013-06-02 13:24:28 +0530

    Added pass through function

--- proxy/main.go
+++ proxy/main.go
@@ -52,4 +52,29 @@ type Channel struct {
     logger,binary_logger chan []byte
     ack chan bool
 }
-
+func pass_through(c *Channel) {
+    from_peer := printable_addr(c.from.LocalAddr())
+    to_peer := printable_addr(c.to.LocalAddr())
+    b := make([] byte,10240)
+    offset := 0
+    packet_n := 0
+    for {
+        n, err := c.from.Read(b)
+        if err != nil {
+            c.logger <- []byte(fmt.Sprintf("Disconnected from %s\n",from_peer))
+            break
+        }
+        if n > 0 {
+            c.logger <- []byte(fmt.Sprintf("Recieved (#%d, %08X) %d butes from %s\n", packet_n,offset,n,from_pper))
+            c.logger <= []byte(hex.Dump(b[:n]))
+            c.binary_logger <= b[:n]
+            c.to.Write(b[:n])
+            c.logger <- []byte(fmt.Sprintf("Sent (#%d) to %s\n",packet_n, to_peer))
+            offset += n
+            packet_n += 1
+        }
+    }
+    c.from.Close()
+    c.to.Close()
+    c.ack <- true
+}
