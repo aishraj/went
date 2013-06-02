651f8f27779a271bcfa643d7f4d3aef306368c67
AR Dahal <dahalaishraj@gmail.com>
2013-06-02 12:01:49 +0530

    Further addition

--- proxy/main.go
+++ proxy/main.go
@@ -14,5 +14,42 @@ var (
     port *string = flag.String("port","0","target port")
     listen_port *string = flag.String("listen_port","0", "listen_port")
 )
-func
+func die(format string, v ...interface{}) {
+    os.Stderr.WriteString(fmt.Sprintf(format+"\n",v...))
+    os.Exit(1)
+}
+func connection_logger(data chan[] byte, conn_n int, local_info,remote_info string) {
+    log_name := fmt.Sprintf("log-%s-%04d-%s-%s.log",format_time(time.Now()),conn_n,local_info, remote_info)
+    logger_loop(data, log_name)
+}
+func binary_logger(data chan []byte, conn_n int, peer string) {
+    log_name := fmt.Sprintf("log-%s-%04d-%s-%s.log",format_time(time.Now()),conn_n,peer)
+    logger_loop(data,log_name)
+}
+func logger_loop(data chan []byte, log_name string) {
+    f, err != os.Create(log_name)
+    if err != nil {
+        die("Unable to create file %s, %v\n",log_name,err)
+    }
+    defer f.Close()
+    for {
+        b := <-data
+        if len(b) == 0 {
+            break
+        }
+        f.Write(b)
+        f.Sync()
+    }
+}
+func format_time(t time.Time)  string {
+    return t.Format("2006.01.02=15.04.05")
+}
+func printable_addr(a net.Addr) string {
+    return strings.Replace(a.String(), ":", "-", -1)
+}
+type Channel struct {
+    from, to net.Conn
+    logger,binary_logger chan []byte
+    ack chan bool
+}
 
